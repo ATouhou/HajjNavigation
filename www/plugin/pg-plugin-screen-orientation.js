6eef263e699fe93e7e9440ec0d3348ccbd3b8174
aildermi <aildermi@yahoo.com>
2013-11-15 02:00:01 +0330

    Added Orientation change plugin

---
+++ www/plugin/pg-plugin-screen-orientation.js
@@ -0,0 +1,1 @@
+var screenOrientation = function() {}screenOrientation.prototype.set = function(str, success, fail) {        cordova.exec(null, null, "ScreenOrientation", "set", [str]);};navigator.screenOrientation = new screenOrientation();
